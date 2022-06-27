df18a2ba09186d8c2b07944ebe4a6db7832cfc50
yasantha <niroshanyi@gmail.com>
2022-06-28 00:12:44 +0530

    adding django setting

--- finnc/settings.py
+++ finnc/settings.py
@@ -155,3 +155,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
