e02da404327dd016aa0d4282bdd964dbd72f9ae2
yasantha <niroshanyi@gmail.com>
2022-06-27 17:24:23 +0530

    ready to publish

--- finnc/finnc/settings.py
+++ finnc/finnc/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
+django_heroku.settings(locals())
 
 from pathlib import Path
 import os
