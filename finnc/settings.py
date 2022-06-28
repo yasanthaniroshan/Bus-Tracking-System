e1515dd0d3f5e9aa71686f01d9dc6561e95025fe
yasantha <niroshanyi@gmail.com>
2022-06-28 08:20:59 +0530

    removing django-heroku

--- finnc/settings.py
+++ finnc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
+
 
 
 from pathlib import Path
@@ -155,4 +155,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
