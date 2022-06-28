ed47a4447484d547cfb78edb1d4177d81462de9a
yasantha <niroshanyi@gmail.com>
2022-06-28 09:22:50 +0530

    changing line

--- finnc/settings.py
+++ finnc/settings.py
@@ -96,7 +96,10 @@ WSGI_APPLICATION = 'finnc.wsgi.application'
 
 
 # Database
+
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 DATABASES = {
     'default': {
@@ -106,7 +109,7 @@ DATABASES = {
 }
 
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
